b4b41702ce61a8e11c48b6d689fb9ea5c0dffc1a
Norman Walsh <ndw@nwalsh.com>
2018-10-18 20:39:27 -0500

    Add test running infrastructure; tweaked Main test driver

--- src/test/scala/com/xmlcalabash/drivers/Main.scala
+++ src/test/scala/com/xmlcalabash/drivers/Main.scala
@@ -1,4 +1,4 @@
-package com.xmlcalabash.ext
+package com.xmlcalabash.drivers
 
 import java.net.URI
 
@@ -7,10 +7,18 @@ import com.xmlcalabash.runtime.{PrintingConsumer, XMLCalabashRuntime}
 import net.sf.saxon.s9api.QName
 
 object Main extends App {
+  val pipeline = new URI(args.head)
+  val document = args(1)
+
   val config = XMLCalabashConfig.newInstance()
-  val runtime: XMLCalabashRuntime = config.runtime(new URI(args.head))
+
+  val runtime: XMLCalabashRuntime = config.runtime(pipeline)
+
+  runtime.option(new QName("", "document"), document)
+
   val serOpt = runtime.serializationOptions("result")
   val pc = new PrintingConsumer(runtime, serOpt)
   runtime.output("result", pc)
+
   runtime.run()
 }
---
+++ src/test/scala/com/xmlcalabash/test/MetaExtSpec.scala
@@ -0,0 +1,7 @@
+package com.xmlcalabash.test
+
+import com.xmlcalabash.util.XProcTestSpec
+
+class MetaExtSpec extends XProcTestSpec {
+  runtests("Run the cx:metadata-extractor test", "src/test/resources/tests")
+}
